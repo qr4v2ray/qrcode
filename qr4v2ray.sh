dddfc17ba56eba16c63fb0e8276c9ba236732208
qr4v2ray <83813080+qr4v2ray@users.noreply.github.com>
2021-05-07 20:21:47 +0800

    Update qr4v2ray.sh

--- qr4v2ray.sh
+++ qr4v2ray.sh
@@ -4,10 +4,10 @@ curl -o code.png $image_url
 code_base64=$(cat code.png |base64 --wrap=0)
 sha_str=$("blob $(printf "$code_base64" | wc -c)\0$code_base64" | sha1sum)
 shasum=$(echo ${sha_str:0:40})
-sha=$(curl -H "Accept: application/vnd.github.v3+json"   https://qr4v2ray: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@api.github.com/repos/qr4v2ray/qrcode/contents/code.png |grep "\"sha\":"|awk -F[\"\"] '{print $4}')
+sha=$(curl -H "Accept: application/vnd.github.v3+json"   https://qr4v2ray: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@api.github.com/repos/qr4v2ray/qrcode/contents/code.png |grep "\"sha\":"|awk -F[\"\"] '{print $4}')
 if [[ "$shasum" != "$sha" ]];then
   echo update
   json="{\"message\":\"message\",\"content\":\"$code_base64\",\"sha\":\"$sha\"}"
   echo $json
-  curl -X PUT -H "Accept: application/vnd.github.v3+json" https://qr4v2ray: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@api.github.com/repos/qr4v2ray/qrcode/contents/code.png -d "$json"
+  curl -X PUT -H "Accept: application/vnd.github.v3+json" https://qr4v2ray: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@api.github.com/repos/qr4v2ray/qrcode/contents/code.png -d "$json"
 fi
